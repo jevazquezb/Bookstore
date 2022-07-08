c2752844f74f282440da702194918c00590389d0
jevazquezb <je.vazquezb@gmail.com>
2022-07-08 01:06:43 -0500

    Add index.css file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 const bookArray = [
   {

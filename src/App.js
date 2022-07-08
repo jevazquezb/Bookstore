8916a7cc60f6ac35e3ba696fc0e021725b31078a
jevazquezb <je.vazquezb@gmail.com>
2022-07-08 00:27:40 -0500

    Add components, routes and prop-types to App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Header from './components/header';
+import Books from './components/books';
+import Categories from './components/categories';
 
-function App() {
+function App({ books }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Books books={books} />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </main>
+    </>
   );
 }
+
+App.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+    id: PropTypes.number,
+  })).isRequired,
+};
 
 export default App;

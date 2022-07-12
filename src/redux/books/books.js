dec71ddb612be529908ba37f4b2b616da41e5f1e
jevazquezb <je.vazquezb@gmail.com>
2022-07-12 13:23:59 -0500

    Add action types, action creators, initial state and reducer in redux/books/books.js file

---
+++ src/redux/books/books.js
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// Actions
+const ADDED_BOOK = 'ADDED_BOOK';
+const REMOVED_BOOK = 'REMOVED_BOOK';
+
+// Action creators
+const addBook = (title, author) => ({
+  type: ADDED_BOOK,
+  title,
+  author,
+  id: uuidv4,
+});
+
+const removeBook = (id) => ({
+  type: REMOVED_BOOK,
+  id,
+});
+
+// Set initial state
+const initialState = [];
+
+// Reducer
+const booksReducer = (state = initialState, action) => {
+  const {type, title, author, id} = action;
+  switch (type) {
+    case ADDED_BOOK:
+      return [
+        ...state,
+        {
+          title,
+          author,
+          id,
+        }
+      ]; 
+    case REMOVED_BOOK:
+      return state.filter((book) => book.id !== id);
+    default:
+      return state;
+  }
+}
+
+export { addBook, removeBook, booksReducer as default };
+

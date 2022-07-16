f561155508c5d49e1954b72b6e8809c439ab278a
jevazquezb <je.vazquezb@gmail.com>
2022-07-15 19:23:03 -0500

    Add styling to the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,18 @@
 import { NavLink } from 'react-router-dom';
+import user from '../images/user.png';
 
 function Header() {
   return (
     <header>
       <h1>Bookstore CMS</h1>
       <nav>
-        <NavLink to="/">Books</NavLink>
+        <NavLink to="/">BOOKS</NavLink>
         <br />
-        <NavLink to="categories">Categories</NavLink>
+        <NavLink to="categories">CATEGORIES</NavLink>
       </nav>
+      <div>
+        <img src={user} alt="user icon" title="Log in" />
+      </div>
     </header>
   );
 }
